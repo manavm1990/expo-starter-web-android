f76c998245656ae72d96dab1579fb9feafdfa0db
Manav Misra <manavm@visionwebsoft.com>
2023-05-23 00:03:29 -0500

    chore: add 'expo router'

---
+++ app/_layout.tsx
@@ -0,0 +1,5 @@
+import { Slot } from 'expo-router';
+
+export default function HomeLayout(): JSX.Element {
+  return <Slot />;
+}
---
+++ app/index.tsx
@@ -0,0 +1,5 @@
+import { Text } from 'react-native';
+
+export default function Home(): JSX.Element {
+  return <Text>Home page</Text>;
+}
