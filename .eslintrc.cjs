eb351181856f8ba09c537f6d48bddedc574faf9e
Manav Misra <manavm@visionwebsoft.com>
2023-05-23 08:42:41 -0500

    chore: add linting for react testing library

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
+    'plugin:testing-library/react',
     'standard-with-typescript',
     'prettier',
   ],
@@ -30,6 +31,18 @@ module.exports = {
       },
     ],
     'no-unused-vars': ['error', { destructuredArrayIgnorePattern: '^_' }],
+
+    // https://callstack.github.io/react-native-testing-library/docs/eslint-plugin-testing-library
+    'testing-library/consistent-data-testid': [
+      2,
+      {
+        testIdAttribute: ['testID'],
+        testIdPattern: '^TestId(__[A-Z]*)?$',
+      },
+    ],
+    'testing-library/no-wait-for-empty-callback': 0,
+    'testing-library/prefer-user-event': 'off',
+    'testing-library/prefer-wait-for': 0,
     'use-encapsulation/prefer-custom-hooks': ['warn', { allow: ['useMemo'] }],
   },
   settings: {
